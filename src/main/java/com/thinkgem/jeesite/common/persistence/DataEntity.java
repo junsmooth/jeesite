3af6db69b44af461d79991e533ace46971d906c1
daijun <junsmooth@163.com>
2014-07-25 08:18:50 +0800

    Date 增加jSON转换注解

--- src/main/java/com/thinkgem/jeesite/common/persistence/DataEntity.java
+++ src/main/java/com/thinkgem/jeesite/common/persistence/DataEntity.java
@@ -102,7 +102,7 @@ public abstract class DataEntity<T> extends BaseEntity<T> implements Serializabl
 	}
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
 	public Date getCreateDate() {
 		return createDate;
 	}
